c748efc5bb999d96cd55f8edfb713d6a83c5fc8b
Therese Lindroth <31589557+thelindr@users.noreply.github.com>
2017-12-04 15:27:50 +0100

    funktioner fungerar

--- code/src/index.js
+++ code/src/index.js
@@ -4,15 +4,62 @@ import "./index.css"
 
 class Clock extends React.Component {
 
+  constructor(props) {
+    super(props)
+    this.state = { date: new Date() }
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    )
+    if (this.state.date > 12) {
+      document.getElementById("contentBox").style.background = "blue"
+    }
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID)
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    })
+  }
+
+  handleClick = () => {
+    clearInterval(this.timerID) // Makes ticking stop
+  }
+
+  handleResumeClick = () => {
+    this.timerID = setInterval( // Resume clock ticking
+      () => this.tick(),
+      1000
+    )
+  }
+
   render() {
     return (
-      <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
+      <div id="contentBox" className="contentBox">
+        <h1>What Time is It?</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+        <div className="buttonBox">
+          <button onClick={this.handleClick}>
+          Timeout
+          </button>
+          <button onClick={this.handleResumeClick}>
+          Resume
+          </button>
+        </div>
       </div>
     )
   }
 
 }
 
-ReactDOM.render(<Clock date={new Date()} />, document.getElementById("root"))
+ReactDOM.render(
+  <Clock />,
+  document.getElementById("root")
+)
